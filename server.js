b807ed74af06e2713971539cd194da055c6ba43f
Srajan-Poojary <srajanpoojary@yahoo.com>
2024-07-06 10:28:28 +0530

    handle cors

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://git-showcase-frontend.vercel.app",
-    ],
+    origin: "https://git-showcase-frontend.vercel.app",
   })
 );
 
